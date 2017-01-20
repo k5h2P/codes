fe752e9250e557e26dfdd6e1256350c506a69741
m1c3m1c3 <qwertyapenguin@gmail.com>
2017-01-20 15:43:34 -0500

    Update mselect.js

--- mselect.js
+++ mselect.js
@@ -1,1 +1,1 @@
-var $=function(a,b){var c=a.slice(0,1),d=a.slice(1);return void 0===b&&(b=0),"#"==c?document.getElementById(d):"."==c?document.getElementsByClassName(d)[b]:document.getElementsByTagName(a)[b]};
+var $=function(a,b){var c=a.slice(0,1),d=a.slice(1);return void 0===b&&(b=0),"#"==c?document.getElementById(d):"."==c?document.getElementsByClassName(d)[b]:document.getElementsByTagName(a)[b]},body={tidy:function(){var a=document.body;a.style.overflow="hidden",a.style.margin=0}};
